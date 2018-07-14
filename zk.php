63f5a99fc351b65b1ce3a477c8e460e191853f7c
bedjan <29821749+bedjan@users.noreply.github.com>
2018-07-14 15:16:56 +0200

    Create zk.php

---
+++ zk.php
@@ -0,0 +1,29 @@
+   <?php 
+
+    echo '<ul id="nav">
+        <li>
+            <a href="Home.html">Home</a>
+        </li>
+        <li>
+            <a>About Me</a>
+            <ul>
+                <li>
+                    <a href="Career.html">Career</a>
+                </li>
+                <li>
+                    <a href="Coding.html">Coding</a>
+                </li>
+                <li>
+                    <a href="Personal.html">Personal</a>
+                </li>
+            </ul>
+        </li>
+        <li>
+            <a href="Travels.html">Travel</a>
+        </li>
+        <li>
+            <a href="Contact.html">Contact</a>
+        </li>
+    </ul>';
+
+    ?>
